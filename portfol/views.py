5500857a53d9e049082ad5ed41e028bc49f6a49b
Resbrotherx <francisdaniel140@gmail.com>
2023-06-05 03:04:16 -0700

    new update

---
+++ portfol/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def service(request):
+    return render(request,'services.html')
+
+def project(request):
+    return render(request,'projects.html')
+
+def contact(request):
+    return render(request,'contact.html')
